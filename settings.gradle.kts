330a7e0355bfa2c6755fb72e8c45f4982cca47d0
arjen <rouvoet@riscure.com>
2024-01-29 20:28:34 +0100

    Add the foojay resolve plugin to provision the jdk

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
 rootProject.name = "apoML"
 
+plugins {
+    // this plugin can auto-provision the required JVM
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
+}
+
 include("apoML")
+
+
